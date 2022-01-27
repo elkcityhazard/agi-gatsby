b6fe1e01dd445e90e57ba2570b59bbcc1612692a
andrew mccall <andrew.m.mccall@gmail.com>
2022-01-27 16:56:50 -0500

    continued about us page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -24,41 +24,71 @@ function AboutUs() {
 
     const { placeholderImage, contentfulAboutUsPage } = useStaticQuery(
         graphql`
-            query {
-        placeholderImage: file(relativePath: {eq: "young-man-carpenter.jpg"}) {
-      childImageSharp {
-        gatsbyImageData(
-          width: 1920
-          breakpoints: [300, 768, 968, 1200, 1600, 1900]
-        )
+  {
+    placeholderImage: file(relativePath: {eq: "young-man-carpenter.jpg"}) {
+        id
+        childImageSharp {
+          gatsbyImageData(width: 1920)
+        }
+      }
+    contentfulAboutUsPage {
+      id
+      slug
+      subtitle
+      publishDate
+      title
+      childContentfulAboutUsPageIntroductionTextNode {
+        introduction
+      }
+      childContentfulAboutUsPageProblemTextNode {
+        id
+      }
+      childContentfulAboutUsPageFutureTextNode {
+        id
+      }
+      futureImg {
+        gatsbyImageData(width: 500)
+      }
+      problemImg {
+        gatsbyImageData(width: 500)
+      }
+      introImg {
+        gatsbyImageData(width: 500)
+      }
+      riseToChallengeImg {
+        gatsbyImageData(width: 500)
+      }
+      solutionsImg {
+        gatsbyImageData(width: 500)
+      }
+      childContentfulAboutUsPageRiseToChallengeTextNode {
+        riseToChallenge
+      }
+      childContentfulAboutUsPageSolutionTextNode {
+        solution
+      }
+      childrenContentfulAboutUsPageFutureTextNode {
+        future
+      }
+      childrenContentfulAboutUsPageIntroductionTextNode {
+        introduction
+      }
+      childrenContentfulAboutUsPageProblemTextNode {
+        problem
+      }
+      childrenContentfulAboutUsPageRiseToChallengeTextNode {
+        riseToChallenge
+      }
+      childrenContentfulAboutUsPageSolutionTextNode {
+        solution
       }
     }
-    contentfulAboutUsPage {
-        id
-        slug
-        subtitle
-        publishDate
-        title
-        childContentfulAboutUsPageIntroductionTextNode {
-          introduction
-        }
-        childContentfulAboutUsPageProblemTextNode {
-          id
-          problem
-        }
-        childContentfulAboutUsPageFutureTextNode {
-          id
-          future
-        }
-        childContentfulAboutUsPageRiseToChallengeTextNode {
-          riseToChallenge
-        }
-        childContentfulAboutUsPageSolutionTextNode {
-          solution
-        }
-      }
+    contentfulAboutUsPageRiseToChallengeTextNode {
+      riseToChallenge
+    }
   }
-`    )
+`
+    )
 
     console.log({ ...contentfulAboutUsPage })
     const image = getImage(placeholderImage)
@@ -111,34 +141,54 @@ function AboutUs() {
                 </Container>
             </BackgroundImage>
             <Container as="main">
-                <Row as="section">
-                    <Col lg={9} className="mx-auto p-3">
-                        <h2 className="h2 text-center">{contentfulAboutUsPage.subtitle}</h2>
+                <Row as="section" className="justify-content-center align-items-center p-5">
+                    <Col lg={6} className="mx-auto p-5">
+                        <h2 className="h2 text-center my-5">{contentfulAboutUsPage.subtitle}</h2>
                     </Col>
                 </Row>
-                <Row as="section">
-                    <Col lg={9} className="mx-auto p-3">
+                <Row as="section" className="justify-content-center align-items-center">
+                    <Col lg={6} className="mx-auto p-3 circle">
+                        <h3>Our History Goes Back 20 Years</h3>
                         {contentfulAboutUsPage.childContentfulAboutUsPageIntroductionTextNode.introduction}
                     </Col>
-                </Row>
-                <Row as="section">
-                    <Col lg={9} className="mx-auto p-3">
-                        {contentfulAboutUsPage.childContentfulAboutUsPageProblemTextNode.problem}
+                    <Col lg={6} className="mx-auto p-3">
+                        <GatsbyImage placeholder="blurred" loading="lazy" layout="constrained" image={contentfulAboutUsPage.introImg.gatsbyImageData} alt={'image'} />
                     </Col>
                 </Row>
-                <Row as="section">
-                    <Col lg={9} className="mx-auto p-3">
+                <Row as="section" className="justify-content-center align-items-center">
+                    <Col lg={6} className="mx-auto p-3">
+                        <GatsbyImage placeholder="blurred" loading="lazy" layout="constrained" image={contentfulAboutUsPage.problemImg.gatsbyImageData} alt={'image'} width={300} />
+                    </Col>
+                    <Col lg={6} className="mx-auto p-3 circle success">
+                        <h3>Our Committment To Integrity</h3>
+                        {contentfulAboutUsPage.childrenContentfulAboutUsPageProblemTextNode[0].problem}
+                    </Col>
+                </Row>
+                <Row as="section" className="justify-content-center align-items-center">
+                    <Col lg={6} className="mx-auto p-3 circle">
+                        <h3>We Are Ready To Rise To The Challenge For You</h3>
                         {contentfulAboutUsPage.childContentfulAboutUsPageRiseToChallengeTextNode.riseToChallenge}
                     </Col>
+                    <Col lg={6} className="mx-auto p-3">
+                        <GatsbyImage placeholder="blurred" loading="lazy" layout="constrained" image={contentfulAboutUsPage.riseToChallengeImg.gatsbyImageData} alt={'image'} />
+                    </Col>
                 </Row>
-                <Row as="section">
-                    <Col lg={9} className="mx-auto p-3">
+                <Row as="section" className="justify-content-center align-items-center">
+                    <Col lg={6} className="mx-auto p-3">
+                        <GatsbyImage placeholder="blurred" loading="lazy" layout="constrained" image={contentfulAboutUsPage.solutionsImg.gatsbyImageData} alt={'image'} />
+                    </Col>
+                    <Col lg={6} className="mx-auto p-3 circle success">
+                        <h3>It All Begins With Listening To You</h3>
                         {contentfulAboutUsPage.childContentfulAboutUsPageSolutionTextNode.solution}
                     </Col>
                 </Row>
-                <Row as="section">
-                    <Col lg={9} className="mx-auto p-3">
-                        {contentfulAboutUsPage.childContentfulAboutUsPageFutureTextNode.future}
+                <Row as="section" className="justify-content-center align-items-center">
+                    <Col lg={6} className="mx-auto p-3 circle">
+                        <h3>We Build For Beauty and Accessibility</h3>
+                        {contentfulAboutUsPage.childrenContentfulAboutUsPageFutureTextNode[0].future}
+                    </Col>
+                    <Col lg={6} className="mx-auto p-3">
+                        <GatsbyImage placeholder="blurred" loading="lazy" layout="constrained" image={contentfulAboutUsPage.futureImg.gatsbyImageData} alt={'image'} />
                     </Col>
                 </Row>
             </Container>
