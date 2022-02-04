5470240c7fcd3a9d253e6f3e94c75f168720369c
andrew mccall <andrew.m.mccall@gmail.com>
2022-02-03 21:03:42 -0500

    worked on blog

---
+++ src/pages/blog.js
@@ -0,0 +1,95 @@
+import React from 'react'
+
+import { graphql, Link, useStaticQuery } from 'gatsby'
+
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
+import ServiceCard from '../components/ServiceCard'
+import ContentfulBlogSlug from './blog/{ContentfulBlogPost.slug}'
+
+import './blog.scss'
+
+
+
+export default function Services({ data }) {
+    const { allContentfulBlogPost: { nodes } } = data
+    console.log(nodes)
+    return (
+        <Container>
+            <Row>
+                <Col sm={12} lg={9} className="mx-auto p-3 text-center">
+                    <h1>Keep Up On DIY Tips & Industry Updates</h1>
+                </Col>
+                <Col sm={12} lg={9} className="mx-auto p-3 text-center">
+                    <p className="lead">🚀 Read all of our expert tips here.  Whether you are DIY 🧙, a professional 👷 of the industry, or want to keep up 🎓
+                        on current construction industry trends, we have it here!</p>
+                </Col>
+            </Row>
+            <Row>
+                {nodes.map((node, index) => (
+                    <Col key={node.id} sm={12} lg={9} className="mx-auto p-3 blog-list">
+                        <Card className="bg-dark text-white p-3 shadow">
+                            <Card.Img src={node.featuredImage.gatsbyImageData.images.fallback.src} title={node.featuredImage.title} alt={node.featuredImage.title} width="300" className="mb-3 shadow" />
+                                <Card.Title><h2 className="h5 fw-bold text-decoration-underline">{node.title}</h2></Card.Title>
+                                <Card.Text className="">
+                                    <small className="d-inline-block pe-3 my-1"><strong>Publish Date:</strong> {new Date(node.publishDate).toLocaleDateString()}</small>
+                                    <small className="d-inline-block pe-3 my-1" ><strong>Author:</strong> {node.author}</small>
+                                    <small className="d-inline-block pe-3 my-1">
+                                        <a as={Link} href="#" aria-role="button" aria-label="category button" link className="btn btn-danger btn-sm">
+                                        {node.category}
+                                        </a>
+                                        </small>
+                                        <small className="d-inline-block pe-3 my-1" ><strong>Tags:</strong> {node.tags.map((tag, index) => {
+                                            return (
+                                                <a as={Link} key={index} href="#" aria-role="button" aria-label="category button" link className="btn btn-info btn-sm me-1 fs">
+                                        {tag}
+                                        </a>
+                                            )
+                                        })}</small>
+                                </Card.Text>
+                                <Card.Text>
+                                    {node.description.description}
+                                </Card.Text>
+                                <Card.Text><Link className="btn btn-warning" to={`/blog/${node.slug}`}>{node.title}</Link></Card.Text>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    )
+
+}
+
+export const data = graphql`
+query getAllBlogPosts {
+    allContentfulBlogPost {
+      nodes {
+        id
+        author
+        category
+        title
+        publishDate
+        featuredImage {
+          title
+          description
+          gatsbyImageData(
+            width: 1200
+            sizes: "300, 499, 768, 968, 1200"
+            quality: 100
+            placeholder: BLURRED
+            breakpoints: [300, 499, 768, 968, 1200, 1500]
+          )
+        }
+        content {
+          content
+        }
+        description {
+          description
+        }
+        slug
+        tags
+      }
+    }
+  }`
---
+++ src/pages/blog/{ContentfulBlogPost.slug}.js
@@ -0,0 +1,102 @@
+import React from 'react';
+
+import { graphql, Link } from 'gatsby';
+
+import BookNow from '../../components/BookNow'
+
+import {GatsbyImage, getImage} from 'gatsby-plugin-image'
+
+import {Container, Row, Col, Button, Form} from 'react-bootstrap'
+
+// Custom CSS
+
+import './ContentfulBlogPost.scss'
+
+export default function ({data}) {
+  console.log(data)
+    const {contentfulBlogPost: {id, author, category, tags, publishDate, content: {content}, description: {description}, featuredImage: {gatsbyImageData}, slug, title}} = data
+
+
+    // Get Image
+
+    const image = getImage(gatsbyImageData)
+  return (
+    <>
+    <header className="p-3" style={{minHeight: `50vh`}}>
+    <Container>
+    <Row>
+    <Col sm={12} xl={6} className="mx-auto p-3">
+    <h1 className="text-uppercase display-2">{title}</h1>
+    <h2>{description}</h2>
+    <div>
+    <small className="d-inline-block pe-3 my-1"><strong>Publish Date:</strong> {new Date(publishDate).toLocaleDateString()}</small>
+                                    <small className="d-inline-block pe-3 my-1" ><strong>Author:</strong> {author}</small>
+                                    <small className="d-inline-block pe-3 my-1">
+                                        <a as={Link} href="#" aria-role="button" aria-label="category button" link className="btn btn-danger btn-sm">
+                                        {category}
+                                        </a>
+                                        </small>
+                                        <small className="d-inline-block pe-3 my-1" ><strong>Tags:</strong> {tags.map((tag, index) => {
+                                            return (
+                                                <a as={Link} key={index} href="#" aria-role="button" aria-label="category button" link className="btn btn-info btn-sm me-1 fs">
+                                        {tag}
+                                        </a>
+                                            )
+                                        })}</small>
+    </div>
+    </Col>
+    <Col sm={12} xl={6} className="mx-auto p-3">
+    <GatsbyImage image={image} alt={description} aria-label="main image" />
+    </Col>
+    </Row>
+    </Container>
+    </header>
+    <Container as="main">
+      <Row className="p-3">
+        <Col as="article" xl={8} className="mx-auto p-3 text-center">
+          <div className="content-wrapper">
+          {content}
+          </div>
+        </Col>
+        <Col as="aside" xl={4}>
+          <BookNow />
+        </Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
+
+export const data = graphql`
+query getSinglePost($slug: String!) {
+    contentfulBlogPost(slug: {eq: $slug}) {
+      id
+      author
+      category
+      tags
+      content {
+        content
+      }
+      description {
+        description
+      }
+      featuredImage {
+        description
+        gatsbyImageData(
+          width: 968
+          sizes: "300, 499, 768, 968, 1200"
+          quality: 100
+          layout: CONSTRAINED
+          breakpoints: [300, 499, 768, 968, 1200]
+          placeholder: BLURRED
+        )
+        title
+      }
+      publishDate
+      slug
+      title
+      updatedAt
+    }
+  }
+`
+
